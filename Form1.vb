29379ed9c3498a569f2763ede1ec4016ab08e0eb
Lenovo-User <Lenovo-User@DESKTOP-KGOC38Q>
2024-01-19 09:32:39 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim a, b, c As Double
+
+        a = Val(TextBox1.Text)
+        b = Val(TextBox2.Text)
+
+        c = (a ^ 2 + b ^ 2) ^ 0.5
+
+        TextBox3.Text = c
+    End Sub
+End Class
